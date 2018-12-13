3e18c16702a60d3c774ea7b888b4f2e393640463
kevinten10 <596823919@qq.com>
2018-12-13 09:19:59 +0800

    Aditum Mocker

--- src/main/java/com/ten/aditum/mocker/AditumMockerApplication.java
+++ src/main/java/com/ten/aditum/mocker/AditumMockerApplication.java
@@ -1,7 +1,5 @@
 package com.ten.aditum.mocker;
 
-import com.ten.aditum.mocker.config.ConfigInit;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +8,8 @@ public class AditumMockerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AditumMockerApplication.class, args);
-
-        ConfigInit configInit = new ConfigInit();
-        configInit.run();
+//
+//        ConfigInit configInit = new ConfigInit();
+//        configInit.run();
     }
 }
