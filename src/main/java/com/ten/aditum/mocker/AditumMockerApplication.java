563b81b9a3fb4496904ba8235068ade4f2495328
Kevin_T <596823919@qq.com>
2019-03-26 14:02:03 +0800

    update

--- src/main/java/com/ten/aditum/mocker/AditumMockerApplication.java
+++ src/main/java/com/ten/aditum/mocker/AditumMockerApplication.java
@@ -1,15 +1,8 @@
 package com.ten.aditum.mocker;
 
 import com.ten.aditum.mocker.config.CommunityMetaInit;
-import com.ten.aditum.mocker.execute.CommunityTablesHolder;
-import com.ten.aditum.mocker.execute.ThreadPoolConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 public class AditumMockerApplication {
